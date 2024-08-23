7b2b1b2e4f0d9ed84236a0b9c44af031957a7b39
vydndrl <vydndrl1234@naver.com>
2024-08-23 11:29:36 +0900

    fix

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -11,7 +11,7 @@
         </v-col>
 
         <v-col class="text-center">
-          <v-btn :to="{ path: '/' }">java shop</v-btn>
+          <v-btn :to="{ path: '/' }">java shop2222222</v-btn>
         </v-col>
         <v-col class="d-flex justify-end">
           <v-btn v-if="isLogin" :to="{ path: '/order/cart' }"
